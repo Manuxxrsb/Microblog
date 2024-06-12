9ecacc6dee21ee5d980649d5aaf399f63f1d99d4
Manuxxrsb <manuxxrsb@gmail.com>
2024-06-11 22:09:36 -0400

    se agrego lo necesario

--- Server/Server.go
+++ Server/Server.go
@@ -1,9 +1,29 @@
 // internal/server/server.go
 package server
 
-import "net/http"
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi"
+)
 
 // Server is a base server configuration.
 type Server struct {
 	server *http.Server
+}
+
+func New(port string) (*Server, error) {
+	r := chi.NewRouter()
+
+	serv := &http.Server{
+		Addr:         ":" + port,
+		Handler:      r,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	server := Server{server: serv}
+
+	return &server, nil
 }
